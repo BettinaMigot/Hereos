71c72e88954708a568deb6f9cbf194e3798484aa
konomori <betti-my@hotmail.fr>
2015-05-05 15:12:53 +0200

    Update connexion_sql.php

--- model/connexion_sql.php
+++ model/connexion_sql.php
@@ -2,7 +2,7 @@
 // Connexion à la base de données
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=heroes2;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=heroes2;charset=utf8', 'root', 'firetoon');
 }
 catch(Exception $e)
 {
